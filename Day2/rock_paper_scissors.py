fd891e1df4f91ad4c2a95e0944b01b9c2c2aa506
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2023-02-19 22:52:28 -0300

    Dia 2, parte 2

--- Day2/rock_paper_scissors.py
+++ Day2/rock_paper_scissors.py
@@ -1,7 +1,13 @@
-OUTCOMES = [
+OUTCOMES1 = [
     "CX", "AY", "BZ",   #Vitória
     "AX", "BY", "CZ",   #Empate
     "BX", "CY", "AZ"    #Derrota
+]
+
+OUTCOMES2 = [
+    "CZ", "AZ", "BZ",   #Vitória
+    "AY", "BY", "CY",   #Empate
+    "BX", "CX", "AX"    #Derrota
 ]
 
 arquivo = open("input.txt", "r")
@@ -13,10 +19,10 @@ pts2 = 0
 
 while linha.__len__() != 0:
     l = linha.split()
+    jogada = l[0] + l[1]
 
     # Parte 1
-    jogada = l[0] + l[1]
-    resultado = OUTCOMES.index(jogada)
+    resultado = OUTCOMES1.index(jogada)
     if resultado <= 2:
         pts1 += 6
     elif resultado <= 5:
@@ -24,8 +30,14 @@ while linha.__len__() != 0:
     pts1 += (resultado % 3) + 1
 
     # Parte 2
-
+    resultado = OUTCOMES2.index(jogada)
+    if resultado <= 2:
+        pts2 += 6
+    elif resultado <= 5:
+        pts2 += 3
+    pts2 += (resultado % 3) + 1
 
     linha = arquivo.readline()
 
 print(pts1)
+print(pts2)
